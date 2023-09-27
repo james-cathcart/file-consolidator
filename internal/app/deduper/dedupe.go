1b78e9b7692f5cf8722ad8c7194c36c92daaa06c
James Cathcart <cathcart.jm@gmail.com>
2023-09-27 13:26:25 -0600

    updated README

--- internal/app/deduper/dedupe.go
+++ internal/app/deduper/dedupe.go
@@ -9,13 +9,13 @@ func New() DeDuper {
 	return &SimpleImpl{}
 }
 
-func (dedupe *SimpleImpl) DeDuplicate(files map[string][]common.FileRecord) (totalCount int, duplicateCount int, unique map[string]common.FileRecord, duplicates map[string][]common.FileRecord) {
+func (dedupe *SimpleImpl) DeDuplicate(files map[string][]common.FileRecord) (output common.DeDupeOutput) {
 
-	totalCount = 0
-	duplicateCount = 0
+	totalCount := 0
+	duplicateCount := 0
 
-	unique = make(map[string]common.FileRecord)
-	duplicates = make(map[string][]common.FileRecord)
+	unique := make(map[string]common.FileRecord)
+	duplicates := make(map[string][]common.FileRecord)
 
 	for k, v := range files {
 		if _, ok := unique[k]; !ok {
@@ -24,6 +24,13 @@ func (dedupe *SimpleImpl) DeDuplicate(files map[string][]common.FileRecord) (tot
 			totalCount += len(duplicates[k]) + 1
 			duplicateCount += len(duplicates[k])
 		}
+	}
+
+	output = common.DeDupeOutput{
+		TotalCount:     totalCount,
+		DuplicateCount: duplicateCount,
+		UniqueFiles:    unique,
+		DuplicateFiles: duplicates,
 	}
 
 	return
--- internal/app/deduper/deduper-abstract.go
+++ internal/app/deduper/deduper-abstract.go
@@ -3,5 +3,5 @@ package deduper
 import "dedupfs/internal/pkg/common"
 
 type DeDuper interface {
-	DeDuplicate(files map[string][]common.FileRecord) (totalCount int, duplicateCount int, unique map[string]common.FileRecord, duplicates map[string][]common.FileRecord)
+	DeDuplicate(files map[string][]common.FileRecord) common.DeDupeOutput
 }
